641a1ddf7fed1f4c2b6674a7267b74627a4ed542
Dario Villar <dario_villar2001@hotmail.com>
2021-10-03 20:46:12 -0300

    Prueba envio de email

---
+++ juego1.js
@@ -0,0 +1,76 @@
+var preguntas = {
+    "1":{
+        "pregunta": "Le preguntamos a 100 daros... su línea de subte favorita",
+        "opciones":[
+            "LÍNEA A", 
+            "LÍNEA H",
+            "LÍNEA D",
+            "LÍNEA B",
+            "LÍNEA C",
+            "LÍNEA E"
+            ],
+        "puntajes":[
+            "28",
+            "16",
+            "13",
+            "8",
+            "6",
+            "4"
+        ]
+    },
+    "2":{
+        "pregunta": "Le preguntamos a 100 daros... cuál es el primer jueguito de celular que se te viene a la cabeza?",
+        "opciones":[
+            "CANDY CRUSH", 
+            "PREGUNTADOS",
+            "POU",
+            "SUBWAY SURFERS",
+            "TEMPLE RUN",
+            "2048"
+            ],
+        "puntajes":[
+            "24",
+            "17",
+            "12",
+            "8",
+            "6",
+            "4"
+        ]
+    },
+    "3":{
+        "pregunta": "Le preguntamos a 100 daros... qué posiciones se pueden jugar en baseball? ",
+        "opciones":[
+            "PITCHER", 
+            "CATCHER",
+            "BATEADOR",
+            "JARDINERO/OUTFIELD",
+            "SEGUNDA BASE",
+            "SHORT/SHORT-STOP",
+            "TERCERA BASE"
+            ],
+        "puntajes":[
+            "31",
+            "16",
+            "10",
+            "8",
+            "7",
+            "5",
+            "4"
+        ]
+    },
+    "4":{
+        "pregunta": "Le preguntamos a 100 delfis... qué es lo que más se vende en Bonitos y Gorditos?",
+        "opciones":[
+            "", 
+            "",
+            "",
+            ""
+            ],
+        "puntajes":[
+            "",
+            "",
+            "",
+            ""
+        ]
+    }
+ }
---
+++ juego8.js
@@ -0,0 +1,186 @@
+var preguntas = {
+    "1":{
+        "pregunta": "Le preguntamos a 100 argentinos... qué dirían cuando dicen 'Doctor, me salió un...'",
+        "opciones":[
+            "GRANO/FORÚNCULO", 
+            "QUISTE",
+            "HEMATOMA/MORETÓN",
+            "BULTO",
+            "HERPES",
+            "SARPULLIDO"
+            ],
+        "puntajes":[
+            "25",
+            "14",
+            "10",
+            "8",
+            "6",
+            "4"
+        ]
+    },
+    "2":{
+    "pregunta": "Le preguntamos a 100 argentinos... qué pensás en el momento de despertarte con 45° de calor?",
+        "opciones":[
+            "ME VOY/SIGO DURMIENDO", 
+            "¡QUÉ FIACA!",
+            "¿QUÉ ME PONGO?",
+            "ME TOMO UN TAXI/REMIS",
+            "QUIERO VACACIONES/FDS",
+            "ME QUIERO MORIR"
+            ],
+        "puntajes":[
+            "24",
+            "16",
+            "9",
+            "7",
+            "6",
+            "4"
+        ]
+    },
+    "3":{
+        "pregunta": "Le preguntamos a 100 argentinos 6 famosas norteamericanas con un lunar en la cara ",
+        "opciones":[
+            "CINDY CRAWFORD",
+            "MARILYN MONROE",
+            "SCARLETT JOHANSSON",
+            "JULIA ROBERTS",
+            "MADONNA",
+            "ANGELINA JOLIE"
+            ],
+        "puntajes":[
+            "23",
+            "17",
+            "11",
+            "7",
+            "6",
+            "4"
+        ]
+    },
+    "4":{
+        "pregunta": "Le preguntamos a 100 argentinos... si no tenés terraza, para qué subirías al techo?",
+        "opciones":[
+            "ARREGLOS/LIMPIEZA", 
+            "BUSCAR PELOTA/GATO",
+            "TOMAR SOL",
+            "COLGAR LA ROPA",
+            "VER ALGO"
+            ],
+        "puntajes":[
+            "30",
+            "12",
+            "9",
+            "7",
+            "6"
+        ]
+    },
+    "5":{
+        "pregunta": "Le preguntamos a 100 argentinos... por qué una pareja mantendría en secreto su relación?",
+        "opciones":[
+            "CASADOS/TIENEN PAREJA", 
+            "DIFERENCIA DE EDAD",
+            "COMPAÑEROS DE TRABAJO",
+            "RECIÉN SEPARADOS",
+            "SON PRIMOS/FAMILIARES",
+            "SON FAMOSOS"
+            ],
+        "puntajes":[
+            "31",
+            "14",
+            "10",
+            "8",
+            "5",
+            "4"
+        ]
+    },
+    "dinerorapido":{
+        "pregunta1": {
+            "pregunta": "¿Qué prenda podés usar, sin que te importe, al revés?",
+            "opciones": [
+                "REMERA/MUSCULOSA",
+                "MEDIAS",
+                "ROPA INTERIOR",
+                "SUÉTER/BUZO"
+            ],
+            "puntajes": [
+                "35",
+                "26",
+                "19",
+                "10"
+            ]
+        },
+        "pregunta2": {
+            "pregunta": "¿A quién llamás si Barassi se queda sin voz?",
+            "opciones": [
+                "ALEJANDRO MARLEY",
+                "GUIDO KACZKA",
+                "MARCELO TINELLI",
+                "JORGE LANATA",
+                "MONCHI BALESTRA",
+                "DIEGO TOPA"
+            ],
+            "puntajes": [
+                "21",
+                "16",
+                "11",
+                "7",
+                "6",
+                "4"
+            ]
+        },
+        "pregunta3": {
+            "pregunta": "Además de comida y bebida, ¿qué ofrecen los vendedores ambulantes en la playa?",
+            "opciones": [
+                "PAREOS/VESTIDOS/BIKINIS",
+                "LENTES DE SOL",
+                "ARTESANÍAS/BIJOU",
+                "GORRAS/SOMBREROS",
+                "SOMBRILLAS",
+                "JUEGOS/JUGUETES"
+            ],
+            "puntajes": [
+                "26",
+                "16",
+                "12",
+                "8",
+                "5",
+                "3"
+            ]
+        },
+        "pregunta4": {
+            "pregunta": "Con una sola palabra, completar la frase 'Tengo ganas de...'",
+            "opciones": [
+                "COMER",
+                "DORMIR",
+                "VIAJAR",
+                "BAILAR",
+                "SALIR/PASEAR",
+                "ENTRENAR"
+            ],
+            "puntajes": [
+                "21",
+                "17",
+                "12",
+                "8",
+                "6",
+                "4"
+            ]
+        },
+        "pregunta5": {
+            "pregunta": "¿Cuándo se puede decir que estás de novio con alguien?",
+            "opciones": [
+                "CONOCÉS FAMILIA/AMIGOS",
+                "DICEN 'TE AMO'",
+                "SOLO SALÍS CON ÉL/ELLA",
+                "DEJA ALGO EN TU CASA",
+                "SUBEN FOTOS JUNTOS",
+            ],
+            "puntajes": [
+                "25",
+                "14",
+                "11",
+                "7",
+                "6"
+            ]
+        }
+    }
+ }
